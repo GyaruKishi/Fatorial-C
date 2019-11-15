a915d6c0f8162988eff27758b428c5741fae8379
Vinícius José Do Valle Ferreira Santos <57399597+VJota108@users.noreply.github.com>
2019-11-15 16:24:58 -0300

    Alteração para utilizar duas threads de teste
    modifiquei para testar com duas threads

--- src/fatorial/c/FatorialC.java
+++ src/fatorial/c/FatorialC.java
@@ -9,6 +9,7 @@ import java.util.Random;
  *
  * @author Luiz Arthur
  * @author Vinícius José Do Valle Ferreira Santos
+ * @author Daniel Nunes
  */
 public class FatorialC {
 
@@ -19,13 +20,7 @@ public class FatorialC {
         ThreadFa tr = new ThreadFa("Thread #1", 250000);
         
         ThreadFa tr2 = new ThreadFa("Thread #2", 250000);
-        
-        ThreadFa tr3 = new ThreadFa("Thread #3", 250000);
-        
-        ThreadFa tr4 = new ThreadFa("Thread #4", 250000);
-        
-        // alterar a parte de threads para demais projetos com 2 e sequencial
-        
+
     }
     
 }
